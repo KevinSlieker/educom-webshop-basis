ec2810e92c0da0ad2cff66e00e45d1f823ac44ce
Kevin Slieker <kevin.slieker.10@gmail.com>
2022-10-13 13:39:53 +0200

    Issue #10

--- validations.php
+++ validations.php
@@ -9,49 +9,37 @@ function validateContact() {
 		$name = test_input(getPostVar('name'));
 		if (empty($name)) {
 			$nameErr = "Naam is verplicht.";
-		} else {
-			if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+		} else if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
 				$nameErr = "Alleen letters en spaties zijn toegestaan.";
-			}
 		}
 
 		$email = test_input(getPostVar('email'));
 		if (empty($email)) {
 			$emailErr = "Email is verplicht.";
-		} else {
-			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 				$emailErr = "Ongeldig email.";
-			}
 		}
 
 		$communication = test_input(getPostVar('communication'));
 		if (empty($communication)) {
 			$communicationErr = "Communicatievoorkeur is verplicht.";
-		} else {
-			
 		}
 
 		$phonenumber = test_input(getPostVar('phonenumber'));
 		if (empty($phonenumber)) {
-			$phonenumberErr = "Telefoonnummer is verplich.t";
-		} else {
-			if (!preg_match("/^0([0-9]{9})$/", $phonenumber)) {
+			$phonenumberErr = "Telefoonnummer is verplicht.";
+		} else if (!preg_match("/^0([0-9]{9})$/", $phonenumber)) {
 				$phonenumberErr = "Vul geldig telefoonnummer in.";
-			}
 		}
 
 		$preamble = test_input(getPostVar('preamble'));
 		if (empty($preamble)) {
 			$preambleErr = "Aanhef is verplicht.";
-		} else {
-	
 		}
 
 		$input = test_input(getPostVar('input'));
 		if (empty($input)) {
 			$inputErr = "";
-		} else {
-		
 		}
 
 		if (empty($nameErr) && empty($emailErr) && empty($communicationErr) && empty($phonenumberErr) && empty($preambleErr) && empty($inputErr)) {
@@ -80,35 +68,27 @@ function validateRegister() {
 		$name = test_input(getPostVar('name'));
 		if (empty($name)) {
 			$nameErr = "Naam is verplicht.";
-		} else {
-			if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+		} else if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
 				$nameErr = "Alleen letters en spaties zijn toegestaan.";
-			}
 		}
 
         $email = test_input(getPostVar('email'));
 		if (empty($email)) {
 			$emailErr = "Email is verplicht.";
-		} else {
-			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 				$emailErr = "Ongeldig email.";
-			}
 		}
 
         $password = test_input(getPostVar('password'));
 		if (empty($password)) {
 			$passwordErr = "Wachtwoord is verplicht.";
-		} else {
-
 		}
 
         $passwordrepeat = test_input(getPostVar('passwordrepeat'));
 		if (empty($passwordrepeat)) {
 			$passwordrepeatErr = "Herhaal wachtwoord is verplicht.";
-		} else {
-         if ($password !== $passwordrepeat) {
+		} else if ($password !== $passwordrepeat) {
 			$passwordrepeatErr = "Herhaal wachtwoord komt niet over een met wachtwoord.";
-		 }
 		}
 		
 		if (empty($nameErr) && empty($emailErr) && empty($passwordErr) && empty($passwordrepeatErr)) {
@@ -142,17 +122,13 @@ function validateLogin() {
         $email = test_input(getPostVar('email'));
 		if (empty($email)) {
 			$emailErr = "Email is verplicht.";
-		} else {
-			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 				$emailErr = "Ongeldig email.";
-			}
 		}
 
         $password = test_input(getPostVar('password'));
 		if (empty($password)) {
 			$passwordErr = "Wachtwoord is verplicht.";
-		} else {
-
 		}
 
         if (empty($emailErr) && empty($passwordErr)) {
